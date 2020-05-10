4a8cc65bcd02d674a93d1a74fd9ceb731887da1a
Adrien BLANCHOT <caboteur.adrienbanchot@gmail.com>
2020-05-11 00:38:19 +0200

    state

--- src/components/introExpo.js
+++ src/components/introExpo.js
@@ -2,19 +2,37 @@ import React, { Component } from "react"
 import { Link } from "gatsby"
 import styles from './introExpo.module.css'
 import img1 from "../images/indigene.jpg"
-import img2 from "../images/indigene-map.png"
-import img3 from "../images/depth.png"
 import title from '../images/title2.svg'
 import enterwebsite from '../images/enterwebsite.svg'
+import { LadderLoading } from 'react-loadingg';
 
 class IntroExpo extends Component {
+  constructor() {
+    super();
+    this.state={
+      load:"inherit"
+    }
+  }
+
+  componentDidMount(){
+    const img = new Image();
+    img.src = img1;
+    img.onload = ()=>{
+      console.log("load")
+      this.setState({load:"none"},console.log(this.state.load))
+    };
+    return;
+  }
 
 render() {
 
     return (
       <>
       <div id="wrap"  className={styles.wrap}>
-      <div className={styles.img}>
+      <div  style={{display:this.state.load}} className={styles.loader}>
+       <LadderLoading stye={{zoom:1.3}} color="#416df7" size="large"></LadderLoading>
+     </div>
+        <div className={styles.img}  style={{backgroundImage: "url(" +  img1 + ")"}}>
       <img id="title"  className={styles.title} src={title} />
       <Link to="/introduction"><img className={styles.button} src={enterwebsite} /></Link>
       </div>
