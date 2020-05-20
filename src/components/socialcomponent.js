4974f9c4011961e90ede571db0a2270a9f946d42
Adrien BLANCHOT <caboteur.adrienbanchot@gmail.com>
2020-05-20 21:29:18 +0200

    logo

--- src/components/socialcomponent.js
+++ src/components/socialcomponent.js
@@ -2,6 +2,7 @@ import React, { Component} from "react"
 import survival from '../images/logo/survival2.svg'
 import humanconet from '../images/logo/humanconet.svg'
 import  igapo from '../images/logo/igapo.svg'
+import  cross from '../images/cross.svg'
 import link from '../images/link.svg'
 import styles from './socialcomponent.module.css'
 import {isMobile} from 'react-device-detect';
@@ -39,9 +40,11 @@ class SocialComponent extends Component {
         <img className={styles.link} onClick={this.handleClick.bind(this)} src={link}></img>
         {this.state.open?
           <div style={{width:this.state.width+"%"}} className={styles.containerlogoMobile}>
+              <img className={styles.logoMobile} onClick={this.handleClick.bind(this)} style={{width:"2rem"}} src={cross} target="_blank"></img>
             <a target="_blank" href="https://www.survivalinternational.fr/"><img className={styles.logoMobile} src={survival} target="_blank"></img></a>
             <a  target="_blank" href='https://igapo-project.jimdofree.com'><img className={styles.logoMobile} src={igapo} target="_blank"></img></a>
             <a  target="_blank" href="https://www.humanconet.org/fr/"><img className={styles.logoMobile} src={humanconet} target="_blank"></img></a>
+
           </div>
            : <p></p>
         }
